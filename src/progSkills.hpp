018b5a3de91f6263022d37c4da1ccaff03ce7a87
Joeger-Bahar <123775427+Joeger-Bahar@users.noreply.github.com>
2023-12-02 16:00:27 -0700

    Fix turnTo function and add printInertial task

--- src/progSkills.hpp
+++ src/progSkills.hpp
@@ -20,6 +20,8 @@ pros::Motor Intake(18, 1); // Reversed
 
 pros::IMU inertial(7);
 
+pros::Controller master(pros::E_CONTROLLER_MASTER);
+
 std::shared_ptr<okapi::OdomChassisController> chassis =
   okapi::ChassisControllerBuilder()
     .withMotors(5, 17, 19, 4)
@@ -30,6 +32,7 @@ std::shared_ptr<okapi::OdomChassisController> chassis =
 
 void turnTo(okapi::QAngle angle, const int speed)
 {
+	master.print(0, 0, "Angle: %f", inertial.get_rotation());
 	int angleValue = angle.getValue() * (180 / PI);
 	// If the desired angle is closer to the current angle on the right, turn right, otherwise turn left
 	if (angleValue - inertial.get_rotation() > 0)
@@ -47,16 +50,16 @@ void turnTo(okapi::QAngle angle, const int speed)
 		BR.move_velocity(speed);
 	}
 
-	while (std::abs(angleValue - inertial.get_rotation()) > 1)
+	while (std::fabs(angleValue - inertial.get_rotation()) > 1)
 	{
 		pros::delay(2);
-		pros::lcd::print(0, "Angle: %f", inertial.get_rotation());
+		//pros::lcd::print(0, "Angle: %f", inertial.get_rotation());
 	}
-	pros::lcd::print(0, "Angle: %f", inertial.get_rotation());
-	FL.move_velocity(0);
-	FR.move_velocity(0);
-	BL.move_velocity(0);
-	BR.move_velocity(0);
+	//pros::lcd::print(0, "Angle: %f", inertial.get_rotation());
+	FL.brake();
+	FR.brake();
+	BL.brake();
+	BR.brake();
 }
 
 double operator""_rotations(long double value)
@@ -69,29 +72,34 @@ double operator""_rotations(uint64_t value)
 	return static_cast<double>(value * 360);
 }
 
+void printInertial(void* ignore)
+{
+	while (1)
+	{
+		pros::lcd::print(0, "Angle: %f", inertial.get_rotation());
+		pros::delay(10);
+	}
+
+}
 
 void ProgrammingSkills(pros::ADIDigitalOut& pneumatics)
 {
-	inertial.reset();
-	while (inertial.is_calibrating())
-	{
-		pros::delay(2);
-	}
+	pros::Task print_inertial(printInertial);
 	// Calibration
 	Time::Timer timer;
 	inertial.set_rotation(180);
 	chassis->setState({0_tile, 0.5_tile, 180_deg});
 
 	// Start match loading
-	FR.move_relative(240, 250);
-	BR.move_relative(240, 250);
+	FR.move_relative(240, 260);
+	BR.move_relative(240, 260);
 	pros::delay(2000);
 	Catapult.move(127);
 	Catapult2.move(63.5);
 	// Times how long we've been match loading
 	timer.Reset();
 
-	while (timer.ElapsedSec() <= 6)
+	while (timer.ElapsedSec() <= 45)
 	{
 		pros::delay(2);
 	}
@@ -102,17 +110,22 @@ void ProgrammingSkills(pros::ADIDigitalOut& pneumatics)
 	//chassis->moveDistance(-0.4_tile);
 
 	chassis->driveToPoint({-1.5_tile, 2_tile}, 1);
+	chassis->setMaxVelocity(600);
 	turnTo(0_deg, 200);
-	chassis->moveDistance(1.75_tile);
-	chassis->setMaxVelocity(120);
-	chassis->moveDistance(-0.2_tile);
-	chassis->moveDistance(0.2_tile);
-	chassis->setMaxVelocity(360);
-	chassis->turnAngle(180_deg);
+	chassis->moveDistance(2_tile);
+	turnTo(180_deg, 200);
 
 	pros::delay(200);
 	pneumatics.set_value(1);
 	pros::delay(200);
 
-	chassis->moveDistance(-1.75_tile);
+	chassis->moveDistanceAsync(-1.2_tile);
+	pros::delay(1500);
+	if (!chassis->isSettled())
+	{
+		chassis->stop();
+	}
+	chassis->moveDistance(1.2_tile);
+	chassis->turnAngle(-30_deg);
+	chassis->moveDistance(-1.2_tile);
 }
